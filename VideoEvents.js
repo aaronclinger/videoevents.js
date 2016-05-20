13cb36a31aaa33ed087f508cd504dacf6eed35d5
Aaron Clinger <aaron@aaronclinger.com>
2016-05-20 18:26:37 -0400

    Initial class.

---
+++ VideoEvents.js
@@ -0,0 +1,35 @@
+/* jshint strict: true, browser: true, nonbsp: true, bitwise: true, immed: true, latedef: true, eqeqeq: true, undef: true, curly: true, unused: true */
+/* global jQuery */
+
+/**
+ * @author Aaron Clinger - https://github.com/aaronclinger/videoevents.js
+ */
+(function($, window) {
+	'use strict';
+	
+	function VideoEvents() {
+		var pub = {};
+		
+		pub.addEvent = function(id, callback) {
+			
+		};
+		
+		pub.removeEvent = function() {
+			
+		};
+		
+		pub.destroy = function() {
+			
+		};
+		
+		var init = function() {
+			
+		};
+		
+		init();
+		
+		return pub;
+	}
+	
+	window.VideoEvents = VideoEvents;
+}(jQuery, window));
